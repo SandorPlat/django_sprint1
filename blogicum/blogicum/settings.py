5c51a17c460b75da158ee0b0878ad99e9df575f4
Александр Сергеев <alexpythonalex@gmail.com>
2024-02-08 23:32:35 +0300

    last

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nl=i^ql&bxwxmcqarh$uq)_-i6567iq9s%(yo!7@b)os#*f3hs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
